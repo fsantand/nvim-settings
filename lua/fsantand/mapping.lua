7cbc057595f7e736435901790309bb41b0364f47
Federico Santander <federico.santander@uber.com>
2023-02-01 20:21:58 -0300

    test: disable arrows

--- lua/fsantand/mapping.lua
+++ lua/fsantand/mapping.lua
@@ -11,3 +11,13 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>")
 keymap.set("n", "<leader>tn", ":tabn<CR>")
 keymap.set("n", "<leader>tp", ":tabp<CR>")
 
+keymap.set("n", "<Left>>", ":echoe 'Use h'<CR>")
+keymap.set("n", "<Right>", ":echoe 'Use l'<CR>")
+keymap.set("n", "<Up>", ":echoe 'Use k'<CR>")
+keymap.set("n", "<Down>",  ":echoe 'Use j'<CR>")
+
+keymap.set("i", "<Left>>", "<ESC>:echoe 'Use h'<CR>")
+keymap.set("i", "<Right>", "<ESC>:echoe 'Use l'<CR>")
+keymap.set("i", "<Up>", "<ESC>:echoe 'Use k'<CR>")
+keymap.set("i", "<Down>",  "<ESC>:echoe 'Use j'<CR>")
+
