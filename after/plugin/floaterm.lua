62e0bb6b1cb7485c3cbf761deafd3bf59f1e0b3a
Federico Santander <federico.santander@uber.com>
2023-02-02 22:31:54 -0300

    add floaterm to packer.lua

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,4 +1,4 @@
 vim.keymap.set("n", "<leader>nt", ":FloatermToggle!<CR>")
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>:FloatermToggle!<CR>")
+vim.keymap.set("t", "<C-Esc>", "<C-\\><C-n>:FloatermToggle!<CR>")
 
 vim.g["floaterm_position"] = "hsplit"
